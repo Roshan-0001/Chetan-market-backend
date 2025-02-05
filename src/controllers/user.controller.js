46b58e39bed5377cc7f28812d1fb2857b2892828
Roshan-0001 <roshan.sharma6383@gmail.com>
2025-02-05 23:16:49 +0530

    remove OTP-related functionality from user controller and routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,7 @@
 import { User } from "../models/user.model.js";
-import nodemailer from 'nodemailer';
 import { AsyncHandler } from "../utills/Asynchandaler.js";
 import { ApiError } from "../utills/ApiError.js";
 import { ApiResponse } from "../utills/ApiResponse.js";
-import { otpStorage } from "../constant.js";
-import OTPGenerator, { generate } from 'otp-generator';
-import mongoose from "mongoose";
-import bcrypt from 'bcrypt';
 import { UploadOnCloudinary } from "../utills/cloudinary.js";
 
 // generating acess token
@@ -93,54 +88,6 @@ const registerUser = AsyncHandler(
         }
     }
 )
-// sends otp to the user end to the email
-const otpSender = AsyncHandler(
-    async (req, res) => {
-        const transporter = nodemailer.createTransport({
-            service: 'gmail', // or another service like 'hotmail', 'yahoo', etc.
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: false,
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.EMAIL_PASSWORD
-            }
-        });
-
-        const email = req.body.email;
-
-        const otp = OTPGenerator.generate(6, { upperCaseAlphabets: false, lowerCaseAlphabets: false, specialChars: false });
-
-
-
-
-        const mailOptions = {
-            from: process.env.EMAIL,
-            to: email,
-            subject: "Your OTP Code",
-            text: `Your OTP is ${otp}`
-        };
-
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.log(error);
-                // throw new ApiError(400, "Error sending otp", error);
-                return res.status(500).json(
-                    new ApiError(500, error, "failed to send otp")
-                );
-
-            } else {
-                res.status(200).json(
-                    new ApiResponse(201, 'otp sended successfully')
-                );
-                otpStorage[email] = otp;
-                console.log('otp sended', otpStorage);
-            }
-        });
-
-
-    }
-)
 
 // for login
 const loginUser = AsyncHandler(async (req, res) => {
@@ -218,115 +165,6 @@ const logoutUser = AsyncHandler(async (req,res) => {
     .json(new ApiResponse(200, {}, "User logged out"))
 })
 
-// verifies the otp given by the user for the email
-const otpVerifier = AsyncHandler(
-    async (req, res) => {
-        const data = req.body;
-        const { email, otp } = data;
-        let user = await User.find({ email }).select("-password");
 
-        if (otpStorage[email] === otp) {
-            delete otpStorage[email];
 
-            await User.updateOne({ _id: user[0]._id }, { $set: { isVerified: true } });
-
-            return res.status(201).json(
-                new ApiResponse(201, user, "user verified successfully")
-            );
-        } else {
-            return res.status(400).json(
-                new ApiResponse(407, this, "invalid otp")
-            );
-        }
-    }
-)
-// login the user with the help of the password
-const passwordLogin = AsyncHandler(
-    async (req, res) => {
-        // take details from the body
-        const { username, password } = req.body;
-
-        //checking for the required details is present or not
-        if (!username || !password) {
-            return res.status(400).json(
-                new ApiError(400, null, "All fields are Required")
-            )
-        }
-
-        //checking the username is present in the database or not
-        const existedUser = await User.findOne({ username });
-
-        if (!existedUser) {
-            return res.status(401).json(
-                new ApiError(401, null, "Invalid credentials")
-            );
-        }
-
-        //checking the given password with the existing password
-
-        const isPasswordValid = await bcrypt.compare(password, existedUser.password);
-        if (!isPasswordValid) {
-            return res.status(401).json(
-                new ApiError(401, null, "Invalid credentials")
-            );
-        }
-
-        const loggedInUser = await User.findById(existedUser._id).select("-password ");
-        return res.status(200).json(
-            new ApiResponse(200, loggedInUser, "user loged in successfully")
-        );
-
-    }
-)
-// login the user with the help of the otp for email
-const otpLogin = AsyncHandler(
-    async (req, res) => {
-        const { username, email } = req.body;
-
-        if (!username && !email) {
-            return res.status(400).json(
-                new ApiError(400, null, "username or email is required for login")
-            );
-        }
-
-        const userExist = await User.findOne({
-            $or: [{ username }, { email }]
-        })
-
-        if (!userExist) {
-            return res.status(400).json(
-                new ApiError(400, null, "invalid credentials")
-            );
-        }
-
-        const mail = { email: userExist.email };
-
-        return res.status(201).json(
-            new ApiResponse(201, mail, "User found")
-        );
-
-    }
-)
-//sends otp to the user end to the number
-// const otpSenderNumber = AsyncHandler(
-//     async(req, res) =>{
-//         const number = req.body.number;
-
-//         // const otp = await generateOTP();
-
-//         const response = await sendOtpToNumber(number);
-
-//         console.log(response);
-//         if(response.ok){
-//             return res.status(200).json(
-//                 new ApiResponse(200, response, "otp sended successfully")
-//             );
-//         } else {
-//             return res.status(500).json(
-//                 new ApiError(500, "failed to send otp", response)
-//             );
-//         }
-
-//     }); 
-
-export { registerUser, otpSender, otpVerifier, passwordLogin, otpLogin, loginUser, logoutUser };
+export { registerUser, loginUser, logoutUser };
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { otpLogin, otpSender, otpVerifier, passwordLogin, registerUser, loginUser, logoutUser } from "../controllers/user.controller.js"
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
@@ -18,15 +18,8 @@ router.route("/register").post(
     ]),
     registerUser
 );
-
-router.route("/register/send-otp").post(otpSender);
-router.route("/register/verify-otp").post(otpVerifier);
-router.route("/login/password-login").post(passwordLogin);
-router.route("/login/otp-login").post(otpLogin);
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT,logoutUser);
-router.route("/login/otp-login/send-otp").post(otpSender);
-router.route("/login/otp-login/verify-otp").post(otpVerifier);
 
 export default router;
 
